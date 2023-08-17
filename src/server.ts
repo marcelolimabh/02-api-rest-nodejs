c3ff381e3ee60f3b03511ef475a16dcd489bb80f
Marcelo Bicalho <marcelolimabh@gmail.com>
2023-08-17 18:34:04 -0300

    Variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async (req, res) => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste 4',
+      amount: 1000,
+    })
+    .returning('*')
+  return transaction
+})
+app.get('/transactions/searchAll', async (req, res) => {
+  const transactions = await knex('transactions').select('*')
+  return transactions
 })
 
 app
